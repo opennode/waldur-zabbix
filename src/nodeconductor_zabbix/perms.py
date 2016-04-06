cbebe733235431720e040f6fbbd00d10e7a6702f
Pavel Marchuk <marchukpavelp@gmail.com>
2016-04-06 10:41:45 +0400

    Fix IT service permissions

--- src/nodeconductor_zabbix/perms.py
+++ src/nodeconductor_zabbix/perms.py
@@ -6,7 +6,7 @@ PERMISSION_LOGICS = (
     ('nodeconductor_zabbix.ZabbixService', structure_perms.service_permission_logic),
     ('nodeconductor_zabbix.ZabbixServiceProjectLink', structure_perms.service_project_link_permission_logic),
     ('nodeconductor_zabbix.Host', structure_perms.resource_permission_logic),
-    ('nodeconductor_zabbix.ITService', StaffPermissionLogic(any_permission=True)),
+    ('nodeconductor_zabbix.ITService', structure_perms.resource_permission_logic),
     ('nodeconductor_zabbix.Template', StaffPermissionLogic(any_permission=True)),
     ('nodeconductor_zabbix.Trigger', StaffPermissionLogic(any_permission=True)),
     ('nodeconductor_zabbix.SlaHistory', StaffPermissionLogic(any_permission=True)),
