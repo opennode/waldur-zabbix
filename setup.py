3f972210420f58de2957cadca5b0f4eae9b2f2e1
Jenkins <jenkins@opennodecloud.com>
2016-08-17 08:59:22 +0000

    Preparing new release: 0.3.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ install_requires = [
 
 setup(
     name='nodeconductor-zabbix',
-    version='0.3.1',
+    version='0.3.2',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='http://nodeconductor.com',
